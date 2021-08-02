5a3b2899289fd1e924473c0f119af8d962452db5
abraham perez reyes <abrahampre27@gmail.com>
2021-08-02 16:47:48 -0500

    added and completed the five day forecast

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,13 +1,15 @@
 var cityInputEl = document.querySelector("#city-name");
 var cityFormEl = document.querySelector("#city-form");
+var fiveDayArea = document.querySelector("#five-day-forecast")
 //daily Displays
 var cityDailyDisplay = document.querySelector("#daily-city");
 var temperatureDailyDisplay = document.querySelector("#daily-temp");
+var humidityDailyDisplay = document.querySelector("#daily-humidity");
 var windDailyDisplay = document.querySelector("#daily-wind");
 var uvDailyDisplay = document.querySelector("#daily-uv");
 var lat= " ";
 var long= " ";
-
+var currentDate = moment().format("L");
 
 var getCityData = function(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=e883e4d86e346084de47b4212521fedc";
@@ -20,14 +22,16 @@ var getCityData = function(city) {
             response.json().then(function(data){
                 console.log(data);
             
-            cityDailyDisplay.textContent=" "+data.name;
+            cityDailyDisplay.textContent=" "+data.name + " " + "("+ currentDate+") ";
             temperatureDailyDisplay.textContent=" "+data.main.temp;
             windDailyDisplay.textContent=" "+data.wind.speed;
+            humidityDailyDisplay.textContent= " "+data.main.humidity;
             
             lat = data.coord.lat;
             console.log(lat);
             long = data.coord.lon;
             console.log(long);
+           
 
             createFiveDays(lat, long)
         });
@@ -52,7 +56,39 @@ var createFiveDays = function(lat, long){
             response.json().then(function(data){
                 console.log(data);
             uvDailyDisplay.textContent = " "+data.current.uvi;
-            
+            for(var i=0; i<5; i++){
+                
+               
+                var futureForecastArea = document.createElement("div");
+                futureForecastArea.classList="list-item card col-md-2 d-flex justify-content-around";
+
+
+                //adding date//
+                var nextFiveForecastDate = document.createElement("h6");
+                nextFiveForecastDate.textContent = moment().add(i+1,'days').format("L");
+                futureForecastArea.appendChild(nextFiveForecastDate);
+                
+
+                var icon= document.createElement("img")
+                icon.setAttribute("src","http://openweathermap.org/img/wn/"+data.daily[i].weather[0].icon  +"@2x.png" )
+                futureForecastArea.appendChild(icon);
+
+                
+                var nextFiveTemperatures = document.createElement("h8");
+                nextFiveTemperatures.textContent = "Temp: " +data.daily[i].temp.day +"°F";
+                futureForecastArea.appendChild(nextFiveTemperatures);
+
+                var nextFiveWind= document.createElement("h8");
+                nextFiveWind.textContent= "Wind: " +data.daily[i].wind_speed + "MPH";
+                futureForecastArea.appendChild(nextFiveWind);
+
+                var nextFiveHum = document.createElement("h8");
+                nextFiveHum.textContent="Humidity: "+data.daily[i].humidity +"%";
+                futureForecastArea.appendChild(nextFiveHum);
+
+                ///last append//
+                fiveDayArea.appendChild(futureForecastArea);
+            } 
         });
         }else{
             alert("Error, City not found!");
