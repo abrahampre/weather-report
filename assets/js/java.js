8ed2e5e8df98e4b0fe02268fd25473cdc3635938
abraham perez reyes <abrahampre27@gmail.com>
2021-08-07 15:28:59 -0500

    buttons added

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,8 +1,12 @@
 var cityInputEl = document.querySelector("#city-name");
 var cityFormEl = document.querySelector("#city-form");
-var fiveDayArea = document.querySelector("#five-day-forecast")
+var fiveDayArea = document.querySelector("#five-day-forecast");
+var cityListArea = document.querySelector("#city-list");
+var selectingCityOnListEl= document.querySelector("#city-list");
+var clickingCity= document.querySelector("#single-city")
 //daily Displays
 var cityDailyDisplay = document.querySelector("#daily-city");
+var imageDailyDisplay = document.querySelector("#daily-image")
 var temperatureDailyDisplay = document.querySelector("#daily-temp");
 var humidityDailyDisplay = document.querySelector("#daily-humidity");
 var windDailyDisplay = document.querySelector("#daily-wind");
@@ -10,6 +14,7 @@ var uvDailyDisplay = document.querySelector("#daily-uv");
 var lat= " ";
 var long= " ";
 var currentDate = moment().format("L");
+var city=" ";
 
 var getCityData = function(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=e883e4d86e346084de47b4212521fedc";
@@ -21,8 +26,12 @@ var getCityData = function(city) {
         if (response.ok){
             response.json().then(function(data){
                 console.log(data);
+                cityDailyDisplay.textContent=" "+data.name + " " + "("+ currentDate+") ";
+                city = data.name
+            // inserting image 
             
-            cityDailyDisplay.textContent=" "+data.name + " " + "("+ currentDate+") ";
+            // imageDailyDisplay.setAttribute("src","http://openweathermap.org/img/wn/"+data.weather[0].icon  +"@2x.png" );
+
             temperatureDailyDisplay.textContent=" "+data.main.temp;
             windDailyDisplay.textContent=" "+data.wind.speed;
             humidityDailyDisplay.textContent= " "+data.main.humidity;
@@ -31,6 +40,7 @@ var getCityData = function(city) {
             console.log(lat);
             long = data.coord.lon;
             console.log(long);
+            
            
 
             createFiveDays(lat, long)
@@ -53,10 +63,17 @@ var createFiveDays = function(lat, long){
     fetch(apiUrlfive).then(function(response){
 
         if (response.ok){
+            
+          
+
             response.json().then(function(data){
                 console.log(data);
             uvDailyDisplay.textContent = " "+data.current.uvi;
+           
+            fiveDayArea.textContent="";
+            
             for(var i=0; i<5; i++){
+                
                 
                
                 var futureForecastArea = document.createElement("div");
@@ -66,6 +83,7 @@ var createFiveDays = function(lat, long){
                 //adding date//
                 var nextFiveForecastDate = document.createElement("h6");
                 nextFiveForecastDate.textContent = moment().add(i+1,'days').format("L");
+               
                 futureForecastArea.appendChild(nextFiveForecastDate);
                 
 
@@ -88,11 +106,13 @@ var createFiveDays = function(lat, long){
 
                 ///last append//
                 fiveDayArea.appendChild(futureForecastArea);
+
             } 
         });
         }else{
             alert("Error, City not found!");
-        };    
+        };   
+    
     })
 };
 
@@ -106,13 +126,41 @@ var cityNameHandler =function(event){
     console.log(event);
 
     var cityName= cityInputEl.value.trim();
+
     console.log (cityName);
     if(cityName){
         getCityData(cityName);
         cityInputEl.value=" ";
+        savingCityList(cityName);
+       
     }else{
         alert("Please type a City")
     }
+
+}
+
+var savingCityList= function(city){
+    var citySavedLi= document.createElement("li")
+    citySavedLi.classlist="btn-block"
+    citySavedLi.setAttribute("id","city-added")
+
+    var citySaved = document.createElement("button");
+    citySaved.classList ="text-uppercase btn btn-primary btn-lg";
+    citySaved.setAttribute("id","single-city");
+    citySaved.setAttribute("type","button");
+    citySaved.textContent = city;
+    //citySaved.addEventListener("onclick", alert("buttonclicked"));
+
+
+    citySavedLi.appendChild(citySaved);
+    cityListArea.appendChild(citySavedLi);  
+}
+    
+
+var runCityOnList=function(){
+    alert("button clicked");
+    console.log(selectingCityOnListEl.textContent);
+   // clickingCity.addEventListener("click",alert("clicked!!!!!!!!!!!"));
 
 }
 
@@ -120,3 +168,4 @@ var cityNameHandler =function(event){
 console.log("outside");
 
 cityFormEl.addEventListener('submit',cityNameHandler);
+selectingCityOnListEl.addEventListener("click", runCityOnList );
